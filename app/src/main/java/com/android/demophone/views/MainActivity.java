94aff9d231beca0bf9279d65af1fb1737355b9ec
Bhim Singh <bhim.singh@squareyards.co.in>
2024-06-25 16:17:18 +0530

    Fixed an emergency bug

--- app/src/main/java/com/android/demophone/views/MainActivity.java
+++ app/src/main/java/com/android/demophone/views/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
         boolean b = PermissionUtils.requestPermissions(this, PermissionUtils.RECORDING_PERMISSION);
 
+        System.out.println("-------- This is major bug fixed ---->");
+
         if(b) {
             startActivity(new Intent(this, SpeechToTextActivity.class));
             finish();
